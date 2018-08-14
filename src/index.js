de4675e7ea8ebab97051cd2d2153cc0bd0c88b92
Stefan Kopco <mail@oddobsessions.com>
2018-08-15 00:08:32 +0200

    Init the project with a somewhat solid base

---
+++ src/index.js
@@ -0,0 +1,102 @@
+const {
+  GraphQLServer
+} = require('graphql-yoga')
+const {
+  Prisma
+} = require('prisma-binding')
+const dotenv = require('dotenv');
+const dotenvExpand = require('dotenv-expand')
+const env = dotenvExpand(dotenv.config()).parsed;
+
+const resolvers = {
+  Query: {
+    posts: (_, args, context, info) => {
+      return context.prisma.query.posts(
+        {
+          where: {
+            OR: [
+              { title_contains: args.searchString },
+              { content_contains: args.searchString },
+            ],
+          },
+        },
+        info,
+      )
+    },
+    user: (_, args, context, info) => {
+      return context.prisma.query.user(
+        {
+          where: {
+            id: args.id,
+          },
+        },
+        info,
+      )
+    },
+  },
+  Mutation: {
+    createDraft: (_, args, context, info) => {
+      return context.prisma.mutation.createPost(
+        {
+          data: {
+            title: args.title,
+            content: args.content,
+            author: {
+              connect: {
+                id: args.authorId,
+              },
+            },
+          },
+        },
+        info,
+      )
+    },
+    publish: (_, args, context, info) => {
+      return context.prisma.mutation.updatePost(
+        {
+          where: {
+            id: args.id,
+          },
+          data: {
+            published: true,
+          },
+        },
+        info,
+      )
+    },
+    deletePost: (_, args, context, info) => {
+      return context.prisma.mutation.deletePost(
+        {
+          where: {
+            id: args.id,
+          },
+        },
+        info,
+      )
+    },
+    signup: (_, args, context, info) => {
+      return context.prisma.mutation.createUser(
+        {
+          data: {
+            name: args.name,
+          },
+        },
+        info,
+      )
+    },
+  },
+}
+
+const server = new GraphQLServer({
+  typeDefs: 'src/schema.graphql',
+  resolvers,
+  context: req => ({
+    ...req,
+    prisma: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: env.PRISMA_ENDPONT,
+    }),
+  }),
+})
+
+server.start(() => console.log(`GraphQL server is running on ${env.SERVER_ENDPONT}`))
